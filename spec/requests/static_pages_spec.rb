189d26ee6c5668dfa9f62f2a59ba829861b8056d
Tim Mulherin <tmulher@bu.edu>
2014-03-11 10:25:10 -0400

    Add titles to Static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "home page" do
+    
+    it "should have the Title Page" do
+      visit '/static_pages/home'
+      expect(page).to have_title('RoR Sample_App |Home|')
+    end
+    
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App') 
@@ -10,6 +16,12 @@ describe "Static pages" do
   end # -> "home page"
 
   describe "help page" do
+
+    it "should have the Title Page" do
+      visit '/static_pages/help'
+      expect(page).to have_title('RoR Sample_App |Help|')
+    end
+
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help') 
@@ -17,6 +29,12 @@ describe "Static pages" do
   end # -> "help page"
   
   describe "about page" do
+
+    it "should have the Title Page" do
+      visit '/static_pages/about'
+      expect(page).to have_title('RoR Sample_App |About|')
+    end
+
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
