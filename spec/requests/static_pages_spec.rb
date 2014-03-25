764fff8948d929d24b7d64dd39f438a2f959acb7
Tim Mulherin <tmulher@bu.edu>
2014-03-25 08:41:17 -0400

    Create base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,26 +3,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "RoR Sample_App" }
+  let(:base_title) { "RoR Sample App" }
   
   describe "home page" do
-    
-    it "should have the Title Page" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} |Home|")
-    end
     
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App') 
     end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("RoR Sample App")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home |')
+    end
+
   end # -> "home page"
+
 
   describe "help page" do
 
     it "should have the Title Page" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} |Help|")
+      expect(page).to have_title("#{base_title} | Help |")
     end
 
     it "should have the content 'Help'" do
@@ -35,7 +42,7 @@ describe "Static pages" do
 
     it "should have the Title Page" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} |About|")
+      expect(page).to have_title("#{base_title} | About |")
     end
 
     it "should have the content 'About Us'" do
@@ -48,7 +55,7 @@ describe "Static pages" do
 
     it "should have the Title Page" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} |Contact|")
+      expect(page).to have_title("#{base_title} | Contact |")
     end
 
     it "should have the content 'contact page'" do
