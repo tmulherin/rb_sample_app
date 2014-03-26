55da429c18be9a861cc3c74fe7c4186dc4ae6b2b
Tim Mulherin <tmulher@bu.edu>
2014-03-26 12:33:45 -0400

    Conclude Chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,7 @@ module ApplicationHelper
   # Returns the full title on a per-page basis
   def full_title(page_title)
     base_title = "RoR Sample App"
-    if page_title.empty?
-      return base_title
-    else
-      return "#{base_title} | #{page_title} |"
-    end
+    return base_title if page_title.empty?
+    return "#{base_title} | #{page_title} |"
   end
 end
